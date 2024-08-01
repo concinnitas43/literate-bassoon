42dd5670bf14d5d5639c74951dd5a02e5c9f72cc
C792 <ians10845@gmail.com>
2024-08-02 02:17:53 +0900

    STFT using librosa

--- src/audio-reader.py
+++ src/audio-reader.py
@@ -1,11 +1,18 @@
-import pywt
-import scipy.io.wavfile
+import numpy as np
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt 
 
+x = librosa.load('audio/test.wav')[0]
+magnitude = np.abs(librosa.stft(x))
+log_spectrogram = librosa.amplitude_to_db(magnitude)
 
-wavefile = 'path to the wavefile'
-# read the wavefile
-sampling_frequency, signal = scipy.io.wavfile.read(wavefile)
-#
-scales = (1, len(signal))
-coefficient, frequency = pywt.cwt(signal, scales, 'wavelet_type')
-
+plt.figure(figsize=(10,4))
+librosa.display.specshow(log_spectrogram)
+plt.xlabel("Time")
+plt.ylabel("Frequency")
+plt.colorbar(format='%+2.0f dB')
+plt.title("Spectrogram (dB)")
+plt.xscale('linear')
+plt.yscale('linear')
+plt.show()
